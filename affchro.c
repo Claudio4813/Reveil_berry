c1561fa77806d41c8d1ac517940f2596ea7f7cb6
fred-isen <74205795+fred-isen@users.noreply.github.com>
2021-11-15 15:46:05 +0100

    Chrono OK

--- affchro.c
+++ affchro.c
@@ -8,23 +8,20 @@
 #include <sys/wait.h>
 #include <signal.h>
 
-    float now=0;
-    int dp;
+float now=0;
+int dp;
 
 void top(int sig)
 {
-    sleep(5);
-    printf("top\n");
-    printf("now=%f\n",now);
-    sleep(5);
-    //dp=open("pipo",O_WRONLY) ; // Ouverture du pipe en ecriture
-    //write(dp, &now ,20);
-    //close(dp);
+
+    dp=open("pipo",O_WRONLY) ; // Ouverture du pipe en ecriture
+    write(dp, &now ,4);
+    close(dp);
 }
 
 void fin(int sig)
 {
-    printf("Fin du chrono");
+    printf("Fin du chrono %d\n",sig);
     exit(0);
 }
 
@@ -32,28 +29,26 @@ void fin(int sig)
 int main(int argc,char * argv[]){
 
     int pidf=getpid();
-    printf("PID affchro %d\n",pidf);
-    sleep(15);
+    int arg=atoi(argv[1]);
+
+    //printf("PID affchro %d\n",pidf);
+    
+    dp=open("pipo",O_WRONLY) ; // Ouverture du pipe en ecriture
+    write(dp, &pidf ,4);
+    close(dp);
+
     signal(SIGUSR1,top); // lancement du top des reception du signal 10
     signal(SIGUSR2,fin); // lancement de la fermeture du programme
 
-
-boucle:
     while(1)
     {
-
         printf("\nCa compte des secondes avec une précision de dizieme de seconde: %.2f",now);
+        printf("\nChronomètre n° %d",arg);
     	usleep(100000);
         now=now+0.1;
-
-    	//system("clear"); //pour terminal systeme linux
+    	system("clear"); //pour terminal systeme linux
         // manière pas optimum car au fur et à mesure du temps on en perds à l'execution du code
         // Il faudrait utiliser l'horloge systeme et calculer la diff pour afficher le temps ecouler
     }
-printf("avant boucle\n");
-sleep(4);
-goto boucle;
-printf("après boucle\n");
-sleep(4);
 }
 
