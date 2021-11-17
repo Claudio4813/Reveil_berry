c945306298622c820b8d831544e2cfccddeebc75
fred-isen <74205795+fred-isen@users.noreply.github.com>
2021-11-17 22:33:50 +0100

    Update make.sh

--- make.sh
+++ make.sh
@@ -5,15 +5,20 @@
 
 echo "installation du Reveil_Berry"
 
-sudo useradd isen -p isen --create-home -U --shell /bin/bash
-su isen isen
+###
+# Essai de creation automatique du profil User ISEN --> No success
+#
+# sudo useradd isen -p isen --create-home -U --shell /bin/bash
+# su isen
+###
+
 sudo apt-get update
 sudo apt-get -y install xterm
 sudo apt-get -y install build-essential
 sudo apt install sox
 
 cd
-mkdir -p /home/ISEN/Reveil_Berry/{bin,src}
+mkdir -p Reveil_Berry/{bin,src}
 
 tar -xzvf Reveil_Berry.tar.gz -C /home/ISEN/Reveil_Berry/src
 
@@ -24,6 +29,8 @@ gcc /home/ISEN/Reveil_Berry/src/affchro.c -o /home/ISEN/Reveil_Berry/bin/affchro
 gcc /home/ISEN/Reveil_Berry/src/MinP.c -o /home/ISEN/Reveil_Berry/bin/MinP -Wall
 gcc /home/ISEN/Reveil_Berry/src/MinF.c -o /home/ISEN/Reveil_Berry/bin/MinF -Wall
 cp /home/ISEN/Reveil_Berry/src/mon.sh home/ISEN/Reveil_Berry/bin/mon.sh
+cp /home/ISEN/Reveil_Berry/src/pan.wav home/ISEN/Reveil_Berry/bin/pan.wav
+
 
 
 echo "S'il n'y a pas de message d'erreur, vous pouvez fermer le terminal. Sinon merci de contacter l'équipe Technique"
